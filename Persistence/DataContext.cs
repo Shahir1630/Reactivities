094b43c7fa01afb7e8ded8aceb801f738baf3f41
Shahir1630 <[16303019@iubat.edu]>
2020-07-03 02:37:26 +0600

    Add project files.

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData
+                (
+                    new Value { Id = 1, Name = "Shahir" },
+                    new Value { Id = 2, Name = "Ayan" },
+                    new Value { Id = 3, Name = "Safin" }
+                );
+        }
+    }
+}
